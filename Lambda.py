83002d05b92ddec84616f32ece8eccac076a93fb
HiemSoheng <hiemsoheng003@gmail.com>
2026-03-03 20:54:29 +0700

    new file

---
+++ Lambda.py
@@ -0,0 +1,11 @@
+#Lambda epuivalent
+from operator import add
+
+
+add_labda = lambda x, y: x + y
+print(add(5, 3))  # Output: 8
+print(add_labda(5, 3))  # Output: 8
+
+
+minus_lambda = lambda x, y: x - y
+print(minus_lambda(10, 4))  # Output: 6
